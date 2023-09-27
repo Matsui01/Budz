179920a0922ceaa53837d8e9dc92b233ebde7711
Matsui <paulo.matsui@mail.tantto.com.br>
2023-09-27 00:06:49 -0300

    dependency injection

---
+++ lib/src/get_it.dart
@@ -0,0 +1,49 @@
+import 'package:budz/src/config/env.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
+
+import 'data/datasource/profile_datasource.dart';
+import 'data/repositories/mock/profile_repository_impl_mock.dart';
+import 'data/repositories/profile_repository_impl.dart';
+import 'domain/repositories/profile_repository.dart';
+import 'utils/constants/strings.dart';
+import 'utils/resources/dio_interceptor.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  switch (EnvInfo.env) {
+    case Env.DEV:
+      _initDev();
+      break;
+    case Env.MOCK:
+      _initMock();
+      break;
+    case Env.PROD:
+      _initDev();
+      break;
+  }
+}
+
+void _initDev() {
+  final dio = Dio();
+  dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true, error: true));
+  dio.interceptors.add(CustomInterceptors());
+  dio.options = BaseOptions(baseUrl: baseUrl);
+
+  getIt.registerSingleton<Dio>(dio);
+  getIt.registerSingleton<Logger>(Logger());
+
+  getIt.registerSingleton<ProfileRepository>(
+    ProfileRepositoryImpl(profileDatasource: ProfileDatasource(getIt<Dio>())),
+  );
+}
+
+void _initMock() {
+  getIt.registerSingleton<Logger>(Logger());
+
+  getIt.registerSingleton<ProfileRepository>(
+    ProfileRepositoryImplMock(),
+  );
+}
